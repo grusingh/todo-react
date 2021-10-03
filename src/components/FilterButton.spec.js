e588bb8c2812730cad03862884cb22bb4ffae36c
Gru Singh <gru.singh@spothero.com>
2021-10-03 17:59:38 -0400

    Unit Testing setup

---
+++ src/components/FilterButton.spec.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {render} from '@testing-library/react';
+import FilterButton from "./FilterButton";
+
+describe("FilterButton", () => {
+    
+    // Happy path: render component for most common use case 
+    it('should render', () => {
+
+    });
+
+    it('should render pressed button', () => {
+
+    });
+
+    it('should render unpressed button', () => {
+
+    });
+
+    it('should be able to set filter', () => {
+
+    });
+});
