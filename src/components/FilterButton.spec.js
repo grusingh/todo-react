3d97484e7771d53702c18fa0a3bf5da30fcc76d2
Gru Sarn <sarngru@gmail.com>
2021-10-09 07:04:31 -0400

    Tests skeleton

--- src/components/FilterButton.spec.js
+++ src/components/FilterButton.spec.js
@@ -1,23 +1,28 @@
 import React from "react";
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import FilterButton from "./FilterButton";
 
 describe("FilterButton", () => {
     
     // Happy path: render component for most common use case 
     it('should render', () => {
-
+        throw 'to be implemented';
     });
 
     it('should render pressed button', () => {
-
+        throw 'to be implemented';
     });
 
     it('should render unpressed button', () => {
-
+        throw 'to be implemented';
     });
 
     it('should be able to set filter', () => {
+        throw 'to be implemented';
+    });
 
+    it('snapshot', () => {
+        throw 'to be implemented';
     });
 });
